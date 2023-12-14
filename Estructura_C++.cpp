4976d10ee2909038a08eacc07588e0c2de4e45b4
Armenteros05 <sarmerod@myuax.com>
2023-12-14 11:53:55 +0100

    Estructura en c++ finalizada

--- Estructura_C++.cpp
+++ Estructura_C++.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 
-// Definición de la estructura Estudiante
 struct Estudiante {
     std::string nombre;
     int edad;
@@ -9,6 +8,17 @@ struct Estudiante {
 };
 void mostrarEstudiante(const Estudiante& estudiante) {
     std::cout << "Nombre: " << estudiante.nombre << std::endl;
-    std::cout << "Edad: " << estudiante.edad << " años" << std::endl;
+    std::cout << "Edad: " << estudiante.edad << "" << std::endl;
     std::cout << "Promedio: " << estudiante.promedio << std::endl;
+}
+int main() {
+    Estudiante estudiante1;
+
+    estudiante1.nombre = "Juan";
+    estudiante1.edad = 20;
+    estudiante1.promedio = 8.5;
+
+    mostrarEstudiante(estudiante1);
+
+    return 0;
 }
