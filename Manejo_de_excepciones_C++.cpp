24c0e9f4792df6f4b38b70e9c812da629845c8e1
Armenteros05 <sarmerod@myuax.com>
2023-12-14 12:26:05 +0100

    update 3

--- Manejo_de_excepciones_C++.cpp
+++ Manejo_de_excepciones_C++.cpp
@@ -26,3 +26,13 @@ struct Estudiante {
     Asistencia asistencias[50];
     int numAsistencias;
 };
+void registrarAsistencia(Estudiante& estudiante, const std::string& fecha,
+                         const std::string& materia, const std::string& estado) {
+    if (estudiante.numAsistencias < 50) {
+        estudiante.asistencias[estudiante.numAsistencias] = {fecha, materia, estado};
+        estudiante.numAsistencias++;
+        std::cout << "Asistencia registrada con exito.\n";
+    } else {
+        throw AsistenciaException("No se pueden registrar mas asistencias. Limite alcanzado.");
+    }
+}
