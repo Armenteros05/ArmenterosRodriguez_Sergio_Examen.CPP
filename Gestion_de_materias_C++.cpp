94ad6a480d614bde7dede5179d500dfc7737e612
Armenteros05 <sarmerod@myuax.com>
2023-12-14 12:00:24 +0100

    update 3

--- Gestion_de_materias_C++.cpp
+++ Gestion_de_materias_C++.cpp
@@ -12,3 +12,9 @@ void mostrarEstudiante(const Estudiante& estudiante) {
     std::cout << "Nombre: " << estudiante.nombre << std::endl;
     std::cout << "Edad: " << estudiante.edad << " años" << std::endl;
     std::cout << "Promedio: " << estudiante.promedio << std::endl;
+    std::cout << "Materias:";
+    for (const std::string& materia : estudiante.materias) {
+        std::cout << " " << materia;
+    }
+    std::cout << std::endl;
+}
