ab545ce7404cbd34abb5658c4c2a32f6c5e5ff9a
Armenteros05 <sarmerod@myuax.com>
2023-12-14 12:13:37 +0100

    update 2

--- Registro_de_asistencia_C++.cpp
+++ Registro_de_asistencia_C++.cpp
@@ -13,3 +13,14 @@ struct Estudiante {
     float promedio;
     std::vector<Asistencia> asistencias;  // Lista de asistencias
 };
+void mostrarEstudiante(const Estudiante& estudiante) {
+    std::cout << "Nombre: " << estudiante.nombre << std::endl;
+    std::cout << "Edad: " << estudiante.edad << " años" << std::endl;
+    std::cout << "Promedio: " << estudiante.promedio << std::endl;
+
+    std::cout << "Asistencias:\n";
+    for (const Asistencia& asistencia : estudiante.asistencias) {
+        std::cout << "Fecha: " << asistencia.fecha << ", Materia: " << asistencia.materia
+                  << ", Estado: " << asistencia.estado << std::endl;
+    }
+}
