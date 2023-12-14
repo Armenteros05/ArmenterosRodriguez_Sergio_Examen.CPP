1a58abc17d1e552af457b5f13d3852e380dd8a02
Armenteros05 <sarmerod@myuax.com>
2023-12-14 12:00:07 +0100

    update 2

--- Gestion_de_materias_C++.cpp
+++ Gestion_de_materias_C++.cpp
@@ -8,3 +8,7 @@ struct Estudiante {
     float promedio;
     std::vector<std::string> materias;  // Lista de materias
 };
+void mostrarEstudiante(const Estudiante& estudiante) {
+    std::cout << "Nombre: " << estudiante.nombre << std::endl;
+    std::cout << "Edad: " << estudiante.edad << " años" << std::endl;
+    std::cout << "Promedio: " << estudiante.promedio << std::endl;
